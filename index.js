13655db93fbac0fcf5e82046f2767f59e1599263
Joe Lafiosca <joe@lafiosca.com>
2015-06-14 16:22:40 -0400

    add 404 catchall for / and temporarily kludge shutdown logic until figuring out how to make it actually work

--- index.js
+++ index.js
@@ -6,6 +6,12 @@ var server;
 
 require(__dirname + '/api/').addRoutes(app, db, '/api');
 
+console.log('adding catchall 404 route for /');
+
+app.use('/', function (req, res, next) {
+    res.sendStatus(404);
+});
+
 db.sequelize.sync().then(function() {
     server = app.listen(port, function() {
         console.log('listening on port ' + port);
@@ -13,19 +19,27 @@ db.sequelize.sync().then(function() {
 });
 
 function shutdown() {
-    console.log('closing express app');
-    server.close(function() {
-        //console.log('closing database');
-        //db.close(function(err) {
-            //if (err) {
-                //console.log('error closing database: ' + err);
-            //} else {
-                //console.log('closed database');
-            //}
+    //console.log('closing database');
+    //db.sequelize.close();
+    //console.log('closing express app');
+    //server.close();
+    console.log('exiting');
+    process.exit(0);
+    /*
+    db.sequelize.close(function(err) {
+        if (err) {
+            console.log('error closing database: ' + err);
+        } else {
+            console.log('closed database');
+        }
+        console.log('closing express app');
+        server.close(function() {
+            console.log('closed express app');
             //console.log('exiting');
             //process.exit(0);
-        //});
+        });
     });
+    */
 }
 
 process.on('SIGTERM', shutdown);
